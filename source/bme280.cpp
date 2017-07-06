3646ca177093352cec68d35b215a93f8716ff75f
Nabil Elqatib <n.elqatib@catie.fr>
2017-07-06 20:02:00 +0200

    Implement I2C helper functions

--- source/bme280.cpp
+++ source/bme280.cpp
@@ -15,27 +15,101 @@
  * limitations under the License.
  */
 
-#include "bme280.h"
+#include "bme280.hpp"
 
-namespace sixtron {
-
-BME280::BME280(I2C * i2c, I2CAddress address):
-        _i2cAddress(address){
-    _i2c = i2c;
-{
-
-bool BME280:initialize(sensor_mode mode)
-{
-    char reg = 0;
-    printf("Initializing BME280 ... \n");
-    i2c_read_register(RegisterAddress::chipID, &reg);
-    if (reg != 0x60) {
-        return false;
-    }
-
-    _chipId = reg
+/*!
+ * @brief Read register data
+ *
+ * @param registerAddress Address of the register
+ * @param value Pointer to the value read from the register
+ *
+ * @return 
+ *         0 on success,
+ *         1 on failure
+ */
+int BME280::i2c_read_register(RegisterAddress registerAddress, int8_t* value){
+	int bus_status = SUCCESS;
+    static char data;
+    data = static_cast<char>(registerAddress);
+    bus_status = _i2c->write(static_cast<int>(_i2cAddress) << 1, &data, 1, true);
+	if (bus_status != 0)
+		return FAILURE;
+	char* char_value = reinterpret_cast<char* >(value);
+    bus_status = _i2c->read(static_cast<int>(_i2cAddress) << 1, char_value, 1);
+	if (bus_status != 0)
+        return FAILURE;
+    return bus_status;
 }
 
+/*!
+ * @brief Read successive registers data
+ *
+ * @note This function is useful to read memory-contiguous LSB/MSB registers
+ *
+ * @param registerAddress Address of the first register
+ * @param value Pointer to the value read from the registers
+ *
+ * @return 
+ *         0 on success,
+ *         1 on failure
+ */
+int BME280::i2c_read_two_bytes(RegisterAddress registerAddress, int16_t* value){
+    int bus_status = SUCCESS;
+    static char data[2];
+    data[0] = static_cast<char>(registerAddress);
+    bus_status = _i2c->write(static_cast<int>(_i2cAddress) << 1, data, 1, true);
+    if (bus_status != 0)
+        return FAILURE;
+    bus_status = _i2c->read(static_cast<int>(_i2cAddress) << 1, data, 2, false);
+    if (bus_status != 0)
+        return FAILURE;
+    *value = (int16_t) (data[1] << EIGHT_BITS_SHIFT) | (0xFF & data[0]);
+    return bus_status;
 }
 
-} // namespace sixtron
+/*!
+ * @brief Read 16 bits signed vector (3 dimensions) continuous read
+ *
+ * @param registerAddress Address of the register containing the LSB 
+ *                        of the first axis
+ *        value Array to store the read values
+ *
+ * @return
+ *         0 on success,
+ *         1 on failure
+ */
+int BME280::i2c_read_vector(RegisterAddress registerAddress, int16_t value[3]){
+    int bus_status = SUCCESS;
+    static char data[6];
+    data[0] = static_cast<char>(registerAddress);
+    bus_status = _i2c->write(static_cast<int>(_i2cAddress) << 1, data, 1, true);
+    if (bus_status != 0)
+        return FAILURE;
+    bus_status = _i2c->read(static_cast<int>(_i2cAddress) << 1, data, 6, false);
+    if (bus_status != 0)
+        return FAILURE;
+    for (int i = 0; i < 3; i++)
+        value[i] = (data[2*i + 1] << EIGHT_BITS_SHIFT) | (0xFF & data[2*i]);
+    return bus_status;
+}
+
+/*!
+ * @brief Write to a register
+ *
+ * @param registerAddress Address of the register to write to
+ * @param value Data to store in the register
+ *
+ * @return 
+ *         0 on success,
+ *         1 on failure
+ */
+int BME280::i2c_write_register(RegisterAddress registerAddress, int8_t value){
+	int bus_status = SUCCESS;
+    static char data[2];
+    data[0] = static_cast<char>(registerAddress);
+    data[1] = static_cast<char>(value);
+    bus_status = _i2c->write(static_cast<int>(_i2cAddress) << 1, data, 2);
+	if (bus_status != 0)
+        return FAILURE;
+    return bus_status;
+}
